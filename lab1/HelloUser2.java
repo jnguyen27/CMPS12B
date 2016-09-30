694924b174298c399d14b3e0bbb533d7d4aa57cd
Duy Minh Nguyen <duminguy@unix2.lt.ucsc.edu>
2016-09-30 15:40:06 -0700

    msg

---
+++ lab1/HelloUser2.java
@@ -0,0 +1,12 @@
+// -------------------------------------
+// Duy Nguyen
+// W1475318
+// CS 12M lab1
+// 09-27-16
+// HelloUser2.java
+// -------------------------------------
+class HelloUser2{
+    public static void main(String[] args){
+        System.out.println("This is lab1 for CS12B.");
+    }
+}
