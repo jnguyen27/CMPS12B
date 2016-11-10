5fb70eafe16b5fc84984411c8c8a4b907f244b28
Duy Minh Nguyen <duminguy@unix3.lt.ucsc.edu>
2016-11-09 23:40:33 -0800

    msg

---
+++ lab5/DictionaryTest.c
@@ -0,0 +1,56 @@
+/*
+ * Duy Nguyen
+ * W1475318
+ * CS 12M lab5
+ * 10/29/16
+ * DictionaryTest.c
+ * Dictionary Test File
+ */
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include"Dictionary.h"
+
+#define MAX_LEN 180
+
+int main(int argc, char* argv[]){
+    Dictionary A = newDictionary();
+    char* k;
+    char* v;
+    char* word1[] = {"1","2","3","4"};
+    char* word2[] = {"one","two","three","four"};
+    int i;
+
+    for(i=0; i<4; i++){
+	insert(A, word1[i], word2[i]);
+    }
+
+    printDictionary(stdout, A);
+
+    for(i=0; i<4; i++){
+	k = word1[i];
+	v = lookup(A, k);
+	printf("key=\"%s\" %s\"%s\"\n", k, (v==NULL?"not found ":"value="), v);
+    }
+
+    delete(A, "1");
+    delete(A, "2");
+
+    printDictionary(stdout, A);
+
+    for(i=0; i<4; i++){
+	k = word1[i];
+	v = lookup(A, k);
+	printf("key=\"%s\" %s\"%s\"\n", k, (v==NULL?"not found ":"value="), v);
+    }
+
+    printf("%s\n", (isEmpty(A)?"true":"false"));
+    printf("%d\m", size(A));
+    makeEmpty(A);
+    printf("%s\n", (isEmpty(A)?"true":"false"));
+
+    freeDictionary(&A);
+
+    return(EXIT_SUCCESS);
+}
